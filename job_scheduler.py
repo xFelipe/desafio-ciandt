185fd42c61412db76cdd7b18e7e3668589cf278e
Felipe Gomes Duarte <xfelipegd@gmail.com>
2021-10-01 21:36:24 -0300

    Jobs priorizados

--- job_scheduler.py
+++ job_scheduler.py
@@ -1,15 +1,23 @@
 from datetime import datetime, timedelta
 
-DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+FORMATO_DE_DATA_E_HORA = '%Y-%m-%d %H:%M:%S'
 
 
-def schedule(jobs, inicio, fim):
-    datetime_inicio = datetime.strptime(inicio, DATETIME_FORMAT)
-    datetime_fim = datetime.strptime(fim, DATETIME_FORMAT)
-    janela_de_tempo = datetime_fim-datetime_inicio
+def schedule(jobs, inicio: str, fim: str):
+    datetime_inicio = datetime.strptime(inicio, FORMATO_DE_DATA_E_HORA)
+    datetime_fim = datetime.strptime(fim, FORMATO_DE_DATA_E_HORA)
+    janela_de_tempo = datetime_fim - datetime_inicio
+
     if janela_de_tempo < timedelta(0):
         raise ValueError(
-            f'Não é possível trabalhar com janela de tempo negativa ({janela_de_tempo}).'
+            'Não é possível trabalhar com '
+            f'janela de tempo negativa ({janela_de_tempo}).'
         )
-    print(janela_de_tempo)
-    
+    jobs_priorizados = sorted(
+        jobs,
+        key=lambda job: datetime.strptime(
+            job['Data Máxima de conclusão'],FORMATO_DE_DATA_E_HORA
+        )
+    )
+
+    conjunto = []
